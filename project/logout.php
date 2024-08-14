471b85025651e6476f0f12aaad4142719d280ae6
Kistone250 <denisquavo004@gmail.com>
2024-08-14 15:31:52 +0200

    Revert "kodesha"

--- project/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require'connect.php';
-echo "<script> alert (' User not found please register')</script>";
-$_SESSION=[];
-session_unset();
-session_destroy();
-header("Location:login.php");
-?>
